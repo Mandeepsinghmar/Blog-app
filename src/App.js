13d01ae48677d333329539537b821c0997e0eba5
Mandeep Singhmar <msigh8328@gmail.com>
2021-03-10 18:45:04 +0530

    add style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
-
-
-import React from 'react'
+import React from 'react';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>js blog</h1>
+    <div className="app">
+      <Navbar />
+
+      <div className="content">
+        <Home />
+       </div>
+  
+       <Footer />
+
     </div>
   )
 }
