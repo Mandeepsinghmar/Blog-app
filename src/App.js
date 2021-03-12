3af226992db71f96f11ea6a792f8fc0f327e39b6
Mandeep Singhmar <msigh8328@gmail.com>
2021-03-12 12:29:03 +0530

    add data file

--- src/App.js
+++ src/App.js
@@ -6,17 +6,31 @@ import Blog from "./components/Blog";
 import BlogProvider from "./BlogProvider";
 
 function App() {
+  const [blogs, setBlogs] = useState(data);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => res.json())
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+
   return (
     <div className="app">
-      <BlogProvider>
-        <Navbar />
+      <Navbar />
 
-        <div className="content">
-          <Blog />
-        </div>
+      <div className="content">
+        <Blog blogs={blogs} handleDelete={handleDelete} />
+      </div>
 
-        <Footer />
-      </BlogProvider>
+      <Footer />
     </div>
   );
 }
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,21 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
-import { BlogsContext } from "../BlogProvider";
-
-function Blog() {
-  const data = useContext(BlogsContext);
-  const [blogs, setBlogs] = useState(data);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-
-    setBlogs(newBlogs);
-  };
-
-  useEffect(() => {
-    console.log("first render");
-    console.log(blogs);
-  }, []);
-
+function Blog({ blogs, handleDelte }) {
   return (
     <div>
       {blogs.map((blog) => (
