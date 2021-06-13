627f7f7947a8f03c47bf667107ba019fcf7abd0d
Mandeep Singhmar <msigh8328@gmail.com>
2021-06-13 12:21:17 +0530

    add: date and location

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { Link } from "react-router-dom";
+import { FaBirthdayCake } from "react-icons/fa";
+import { GoLocation } from "react-icons/go";
+import moment from "moment";
 
 const UserProfile = ({ id, User }) => {
   const [user, setUser] = useState(null);
+  const [joinedDate, setJoinedDate] = useState("");
+
+  const fetchDate = () => {
+    if (user) {
+      if (user.createdAt !== null) {
+        setTimeout(() => {
+          const date = new Date(
+            user.createdAt.seconds * 1000
+          ).toLocaleDateString();
+          setJoinedDate(date);
+        }, 1000);
+      }
+    }
+  };
+  fetchDate();
+
   useEffect(() => {
     db.collection("users").onSnapshot((snap) => {
       snap.forEach((doc) => {
@@ -50,9 +69,49 @@ const UserProfile = ({ id, User }) => {
           />
           <p>{user.displayName}</p>
           <p>{user.bio}</p>
-          <a href={user.website} target="_blank" rel="noreferrer nofollow">
+
+          <div style={{ display: "flex", gap: "20px" }}>
+            <div>
+              {user.location && (
+                <>
+                  {" "}
+                  <GoLocation />
+                  <span style={{ fontSize: "0.9rem" }}>{user.location}</span>
+                </>
+              )}
+            </div>
+            <div>
+              {/* {date ? <p>{date}</p> : ""} */}
+              {joinedDate ? (
+                <>
+                  <FaBirthdayCake />{" "}
+                  <span style={{ fontSize: "0.9rem" }}>
+                    Joined on{" "}
+                    {joinedDate && moment(joinedDate).format("MMM D, YYYY")}
+                  </span>
+                </>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+          <a
+            href={user.website}
+            target="_blank"
+            rel="noreferrer nofollow"
+            style={{ textDecoration: "none", color: "blue" }}
+          >
             {user.website && user.website.substr(8)}
           </a>
+          <div>
+            {" "}
+            {user.work && (
+              <>
+                <strong>Work as</strong>
+                <span> {user.work}</span>
+              </>
+            )}
+          </div>
           {User && User.uid === user.uid && (
             <div>
               <button>
